c35522b2283b0a0663cb7be67519415bfebf93ec
Manuell00 <manuell.caselli@gmail.com>
2023-07-20 15:58:01 +0200

    Creazione del model e inserimento dati

---
+++ app/Models/Project.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    // Inserisco i dati da proteggere
+    protected $fillable = [
+        'project_name',
+        'description',
+        'start_date',
+        'status',
+        'budget',
+        'progress'
+    ];
+}
