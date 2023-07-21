dc65568c3a06d88e1e1b4f7d349df8c87a460d97
Manuell00 <manuell.caselli@gmail.com>
2023-07-21 10:19:06 +0200

    Creo i controller per i log e per i guest

---
+++ app/Http/Controllers/Guest/ProjectController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Importo il model
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    //
+}
