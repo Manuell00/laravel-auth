db4daf0f0838fc962e8890157aa633c34c973715
Manuell00 <manuell.caselli@gmail.com>
2023-07-24 15:11:04 +0200

    Creazione della show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
     <div class="container text-center">
-        <h1>Hello wprld from show</h1>
+
+        {{-- Versione UTENTE --}}
+        @auth
+            <ol class="list-unstyled">
+                <li> <b>Id :</b> {{ $project -> id}}</li>
+                <br>
+                <li> <b>Nome del progetto : </b>{{ $project -> project_name}}</li>
+                <br>
+                <li> <b> Descrizione : </b>{{ $project -> description}}</li>
+                <br>
+                <li> <b>start_date : </b>{{ $project ->start_date }}</li>
+                <br>
+                <li> <b>end_date : </b>{{ $project ->end_date }}</li>
+                <br>
+                <li> <b>status : </b>{{ $project ->status }}</li>
+                <br>
+                <li> <b>budget : </b>{{ $project ->budget }}</li>
+                <br>
+                <li> <b>progress : </b>{{ $project ->progress }}</li>
+            </ol>
+       
+
+        @endauth
     </div>
 @endsection
